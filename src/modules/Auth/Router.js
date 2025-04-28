378f89fd910d84aafe7e9f5153fb9d1029f5b98e
joregesosa <joregesosa@gmail.com>
2025-04-28 11:35:11 -0400

    fix: add authentication middleware to change-password route

--- src/modules/Auth/Router.js
+++ src/modules/Auth/Router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { changePassword, login, register, logout } from "./Controller.js";
+import authMiddleware from "#src/middlewares/authMiddleware.js";
 
 export const authRouter = Router();
 
 authRouter.post("/login", login);
-authRouter.put("/change-password", changePassword);
+authRouter.put("/change-password", authMiddleware, changePassword);
 authRouter.post("/register", register);
 authRouter.get("/logout", logout);
 
